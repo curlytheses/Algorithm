7716cd1efe1a4bd8412a3df69210748a7c4525b9
curlytheses <rdas26244@gmail.com>
2022-11-30 06:49:30 +0530

    commit

--- MergeSort.c
+++ MergeSort.c
@@ -1,10 +1,7 @@
-// Merge sort in C
 #include <stdio.h>
 #define MAX 100
-// Merge two subarrays L and M into arr
-void merge(int arr[], int p, int q, int r) 
+void merge(int arr[], int p, int q, int r)
 {
-  // Create L ← A[p..q] and M ← A[q+1..r]
   int n1 = q - p + 1;
   int n2 = r - q;
   int L[n1], M[n2];
@@ -12,73 +9,64 @@ void merge(int arr[], int p, int q, int r)
     L[i] = arr[p + i];
   for (int j = 0; j < n2; j++)
     M[j] = arr[q + 1 + j];
-  // Maintain current index of sub-arrays and main array
   int i, j, k;
   i = 0;
   j = 0;
   k = p;
-  // Until we reach either end of either L or M, pick larger among
-  // elements L and M and place them in the correct position at A[p..r]
-  while (i < n1 && j < n2) 
+  while (i < n1 && j < n2)
   {
-    if (L[i] <= M[j]) 
+    if (L[i] <= M[j])
     {
       arr[k] = L[i];
       i++;
-    } else 
+    }
+    else
     {
       arr[k] = M[j];
       j++;
     }
     k++;
   }
-  // When we run out of elements in either L or M,
-  // pick up the remaining elements and put in A[p..r]
-  while (i < n1) 
+  while (i < n1)
   {
     arr[k] = L[i];
     i++;
     k++;
   }
-  while (j < n2) 
+  while (j < n2)
   {
     arr[k] = M[j];
     j++;
     k++;
   }
 }
-// Divide the array into two subarrays, sort them and merge them
-void mergeSort(int arr[], int l, int r) 
+void mergeSort(int arr[], int l, int r)
 {
-  if (l < r) 
+  if (l < r)
   {
-    // m is the point where the array is divided into two subarrays
     int m = l + (r - l) / 2;
     mergeSort(arr, l, m);
     mergeSort(arr, m + 1, r);
-    // Merge the sorted subarrays
     merge(arr, l, m, r);
   }
 }
-// Print the array
-void printArray(int arr[], int size) 
+void printArray(int arr[], int size)
 {
   for (int i = 0; i < size; i++)
     printf("%d ", arr[i]);
   printf("\n");
 }
-// Driver program
-int main() 
+int main()
 {
-  	int arr[MAX],i,size;
-	printf("Enter the number of elements to be sorted:");
-	scanf("%d",&size);
-	for ( i = 0; i < size; i++)
-	{
-		printf("Enter the elements:");
-		scanf("%d",&arr[i]);
-	}	
-	
+  int arr[MAX], i, size;
+  printf("Enter the number of elements to be sorted:");
+  scanf("%d", &size);
+  for (i = 0; i < size; i++)
+  {
+    printf("Enter the elements:");
+    scanf("%d", &arr[i]);
+  }
+
   mergeSort(arr, 0, size - 1);
   printf("Sorted array: \n");
   printArray(arr, size);
--- QuickSort.c
+++ QuickSort.c
@@ -1,15 +1,11 @@
-// An iterative implementation of quick sort
 #include <stdio.h>
 #define MAX 100
-
-// A utility function to swap two elements
 void swap(int* a, int* b)
 {
 	int t = *a;
 	*a = *b;
 	*b = t;
 }
-/* This function is same in both iterative and recursive*/
 int partition(int arr[], int l, int h)
 {
 	int x = arr[h];
@@ -25,36 +21,22 @@ int partition(int arr[], int l, int h)
 	swap(&arr[i + 1], &arr[h]);
 	return (i + 1);
 }
-/* A[] --> Array to be sorted,
-l --> Starting index,
-h --> Ending index */
 void quickSortIterative(int arr[], int l, int h)
 {
-	// Create an auxiliary stack
 	int stack[h - l + 1];
-	// initialize top of stack
 	int top = -1;
-	// push initial values of l and h to stack
 	stack[++top] = l;
 	stack[++top] = h;
-	// Keep popping from stack while is not empty
 	while (top >= 0) 
 	{
-		// Pop h and l
 		h = stack[top--];
 		l = stack[top--];
-		// Set pivot element at its correct position
-		// in sorted array
 		int p = partition(arr, l, h);
-		// If there are elements on left side of pivot,
-		// then push left side to stack
 		if (p - 1 > l) 
 		{
 			stack[++top] = l;
 			stack[++top] = p - 1;
 		}
-		// If there are elements on right side of pivot,
-		// then push right side to stack
 		if (p + 1 < h) 
 		{
 			stack[++top] = p + 1;
@@ -62,14 +44,12 @@ void quickSortIterative(int arr[], int l, int h)
 		}
 	}
 }
-// A utility function to print contents of arr
 void printArr(int arr[], int n)
 {
 	int i;
 	for (i = 0; i < n; ++i)
 		printf("%d ", arr[i]);
 }
-// Driver program to test above functions
 int main()
 {
 	int arr[MAX],i,n;
