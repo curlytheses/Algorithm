d51c743d7b9ada3d1f0b5e849103eae1ecf261bf
curlytheses <rdas26244@gmail.com>
2022-11-30 07:00:01 +0530

    SAVE

--- Memoization.c
+++ Memoization.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-// C program to find the Nth term
-// of Fibonacci series
-// Fibonacci Series using Recursion
 int fibo(int n)
 {
-	// Base case
 	if (n <= 1)
 		return n;
-    // recursive calls
     else
 	    return fib(n - 1) + fib(n - 2);
 }
-// Driver Code
 int main()
 {
 	int n,i;
